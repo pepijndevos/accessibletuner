49ca79585c49953dae7a0ca9c2bd814df4311dfb
Pepijn de Vos <pepijndevos@gmail.com>
2017-07-10 17:44:13 +0200

    Working things using envelope detection

--- tuner/tuner.ino
+++ tuner/tuner.ino
@@ -1,5 +1,4 @@
 #include "trig8.h"
-#include "iir.h"
 #include <limits.h>
 
 #define SEMITONE 1.059463
@@ -18,46 +17,24 @@
 #define A2 (110.00*OMEGA)
 #define E2 (82.41*OMEGA)
 
-// how tuned is tuned? freq/MARGIN
-#define MARGIN 300
-
 #define BUZZER 10
-#define CLICKER 9
+#define ENVELOPE 9
+#define CLICKER 8
 #define BUTTON 3
 
 volatile uint16_t buf[256];
-volatile unsigned int counter = 0;
+volatile unsigned long long counter = 0;
 
 const float strings[6] = {E2, A2, D3, G3, B3, E4};
 const char names[6][2] = {"E", "A", "D", "G", "B", "E"};
 int note_idx = 3;
-
-
-/**********************************************************/
-// Filter coefficients
-/**********************************************************/
-/* These coefficients will give the response of a low pass filter.
- * A0 is not defined since it is the downscaling factor.
- *
- * Note that the sign is changed on the A*-coefficients, relative to the ones
- * presented in the appplication note. This is because only additions are used
- * in the filtering algorithm, while the difference equation requires
- * subtractions for the feedback-part.
- */
-#define FB0    2072   //!< First feed-forward coefficient.
-#define FB1   -4143   //!< Second feed-forward coefficient.
-#define FB2    2072   //!< Third feed-forward coefficient.
-#define FA1    130961 //!< Second feedback coefficient.
-#define FA2   -65425 //!< Third feedback coefficient.
-
-// Initialize the filter struct.
-struct IIR_filter lpf = {0,0,0,0, FB0, FB1, FB2, FA1, FA2};
 
 void setup(){
   
   Serial.begin(9600);
   
   pinMode(BUZZER, OUTPUT);
+  pinMode(ENVELOPE, OUTPUT);
   pinMode(CLICKER, OUTPUT);
   pinMode(BUTTON, INPUT);
     
@@ -77,11 +54,12 @@ void setup(){
          |  (1 << ADSC); //start ADC measurements
 
   // Enable output A, B, fast PWM
-  TCCR2A = _BV(COM2A1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
   // No PWM prescaler
   TCCR2B = _BV(CS20);
   // Duty cycle
   OCR2A = 127;
+  OCR2B = 127;
 }
 
 SIGNAL(ADC_vect) {//when new ADC value ready
@@ -90,15 +68,22 @@ SIGNAL(ADC_vect) {//when new ADC value ready
   uint8_t val = ADCH;
   float f = strings[note_idx];
   uint8_t sine = sin8(f*counter);
+  //int16_t prod = (val*sine)-0x7fff;
+  //int16_t enve = IIR2(&lpf, prod);
   OCR2A = sine;
-  int16_t prod = (val*sine)-0x7fff;
-  int16_t enve = IIR2(&lpf, prod);
-  digitalWrite(CLICKER, enve>0);
-  //int idx = counter & 0xff;
-  //buf[idx] = val*sine;
+  //digitalWrite(CLICKER, enve>0);
+  int idx = counter & 0xff;
+  buf[idx] = val*sine;
   counter++;
 }
 
 void loop() {
-    Serial.println("hello");
+    uint16_t m = 0;
+    for(int i=0; i<256; i++) {
+      if (buf[i] > m) {
+        m = buf[i];
+      }
+    }
+    OCR2B = m>>8;
+    digitalWrite(CLICKER, m>0x7fff);
 }
