b6a555a2e73a4d636997e86f9da23e6fd7ee409f
Pepijn de Vos <pepijndevos@gmail.com>
2018-02-03 20:16:00 +0100

    update for hardware rev3

--- tuner/tuner.ino
+++ tuner/tuner.ino
@@ -1,3 +1,5 @@
+#include <SPI.h>
+
 #include "trig8.h"
 #include <limits.h>
 
@@ -7,9 +9,9 @@
 // CPU_F/prescaler/ADC_cycles
 // 160000000/128/13
 // Adjust to tune the tuner
-#define ADCFREQ 9575.0
+#define ADCFREQ 1953.128
 // scales to the range of sin8: 0-255
-#define OMEGA (255/ADCFREQ)
+#define OMEGA (256/ADCFREQ)
 
 #define E4 (329.63*OMEGA)
 #define B3 (246.94*OMEGA)
@@ -18,7 +20,9 @@
 #define A2 (110.00*OMEGA)
 #define E2 (82.41*OMEGA)
 
-#define BUZZER 3
+#define BUZZER 9
+#define EN_OUT 3
+#define ADC_CS A1
 
 // PORTB
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
@@ -29,28 +33,29 @@
 #define SCROLL_COUNTER 7
 #endif
 
-#define BUFSIZE (1<<8)
+#define BUFSIZE (1<<6)
 #define BUFMASK (BUFSIZE-1)
 volatile uint16_t buf[BUFSIZE];
 volatile unsigned long long counter = 0;
 
-#define ENVSIZE (1<<7)
+#define ENVSIZE (1<<8)
 #define ENVMASK (ENVSIZE-1)
 uint16_t env_buf[ENVSIZE];
 uint8_t env_counter = 0;
 
 const float strings[6] = {E2, A2, D3, G3, B3, E4};
 //const char names[6][2] = {"E", "A", "D", "G", "B", "E"};
-int note_idx = 0;
+int note_idx = 4;
 //volatile float note = D3;
 volatile uint8_t phase = 0;
 
 void setup(){
-  
   Serial.begin(115200);
+  SPI.begin();
   
+  pinMode(ADC_CS, OUTPUT);
   pinMode(BUZZER, OUTPUT);
-
+  pinMode(EN_OUT, OUTPUT);
     // rotary encoder
   pinMode(SCROLL_PHASE, INPUT);
   pinMode(SCROLL_COUNTER, INPUT);
@@ -59,42 +64,37 @@ void setup(){
   PCMSK2 = (1 << PCINT22) | (1 << PCINT23);  // Mask for encoder pins
     
   //set up continuous sampling of analog pin 0 at 10kHz
- 
-  //clear ADCSRA and ADCSRB registers
-  ADCSRA = 0;
-  ADCSRB = 0;
-  
-  ADMUX |= (1 << REFS0) //set reference voltage
-        |  (1 << ADLAR); //left align the ADC value- so we can read highest 8 bits from ADCH register only
-  
-  ADCSRA |= (1 << ADPS2) | (1 << ADPS1)| (1 << ADPS0) //set ADC clock with 128 prescaler- 16mHz/128=125kHz
-         |  (1 << ADATE) //enabble auto trigger
-         |  (1 << ADIE) //enable interrupts when measurement complete
-         |  (1 << ADEN) //enable ADC
-         |  (1 << ADSC); //start ADC measurements
-
+  // Timer0 is already used for millis() this breaks everything
+  TCCR0A =  _BV(WGM01);
+  TCCR0B = _BV(CS01) | _BV(CS00);
+  OCR0A = 127;
+  TIMSK0 |= _BV(OCIE0A);
+      
   // Enable output A, B, fast PWM
-  TCCR2A =  _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR1A =  _BV(COM1A1) | _BV(WGM10);
   // No PWM prescaler
-  TCCR2B = _BV(CS20);
+  TCCR1B = _BV(CS10) | _BV(WGM12);
   // Duty cycle
-  OCR2A = 127;
-  OCR2B = 127;
+  OCR1A = 127;
+  digitalWrite(EN_OUT, HIGH);
 
 }
 
-SIGNAL(ADC_vect) {//when new ADC value ready
-  //uint16_t val = ADCL;
-  //val |= ADCH << 8;
-  uint8_t val = ADCH;
+SIGNAL(TIMER0_COMPA_vect)  {//when new ADC value ready
+  SPI.beginTransaction(SPISettings(F_CPU/2, MSBFIRST, SPI_MODE0));
+  digitalWrite(A1, LOW);
+  delayMicroseconds(1);
+  uint16_t val = SPI.transfer16(0)<<4;
+  digitalWrite(A1, HIGH);
+  SPI.endTransaction();
+  
   float note = strings[(note_idx/4) % 6];
-  uint8_t sine = sin8(note*counter);
-  //int16_t prod = (val*sine)-0x7fff;
-  //int16_t enve = IIR2(&lpf, prod);
-  OCR2B = sin8(note*counter+phase)/64;
+  uint16_t sine = sin16(note*256*counter)^(1<<15);
+  OCR1A = sin8(note*counter+phase);
   //digitalWrite(CLICKER, enve>0);
   int idx = counter & BUFMASK;
-  buf[idx] = val*sine;
+  uint32_t prod = (uint32_t)(uint16_t)val*(uint32_t)(uint16_t)sine;
+  buf[idx] = prod>>16;
   counter++;
 }
 
@@ -152,8 +152,10 @@ void loop() {
     if (env_max-env_min > 5000) {
       phase = (m>threshold)*128;
     }
-    //int idx = counter & BUFMASK;
+    int idx = counter & BUFMASK;
     Serial.print(m, DEC);
+    Serial.print("\t");
+    Serial.print(buf[idx], DEC);
     Serial.print("\t");
     Serial.print(env_min, DEC);
     Serial.print("\t");
